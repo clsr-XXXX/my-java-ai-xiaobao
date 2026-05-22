d69b1f85ba56ec655de70ff2a75e1f231ab99641
gml <1821196736@qq.com>
2026-05-22 10:24:03 +0800

    口腔科 小包

--- src/main/java/org/lc4j/controller/XiaoBaoController.java
+++ src/main/java/org/lc4j/controller/XiaoBaoController.java
@@ -3,9 +3,8 @@ package org.lc4j.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.lc4j.assistant.XiaoBaoAgent;
 import org.lc4j.bean.ChatForm;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.lc4j.service.AgentRouterService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class XiaoBaoController {
 
-    @Autowired
-    private XiaoBaoAgent xiaoBaoAgent;
+    private final AgentRouterService agentRouterService;
+
+    public XiaoBaoController(AgentRouterService agentRouterService) {
+        this.agentRouterService = agentRouterService;
+    }
 
     @Operation(summary = "对话")
     @PostMapping("/chat")
     public String chat(@RequestBody ChatForm chatForm) {
         log.info("开始对话");
-        return xiaoBaoAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
+        return agentRouterService.chat(chatForm.getMemoryId(), chatForm.getMessage());
     }
 }
